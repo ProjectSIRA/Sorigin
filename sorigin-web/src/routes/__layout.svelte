dcba458aaa6cc9172ff42806fd6c6b831211ae9b
Auros Nexus <aurosnex@gmail.com>
2021-08-17 03:17:49 -0400

    Added Site Description

--- sorigin-web/src/routes/__layout.svelte
+++ sorigin-web/src/routes/__layout.svelte
@@ -13,6 +13,7 @@
 	@media only screen and (prefers-color-scheme: dark) {
 		:root:not([data-theme="light"]) {
             --background-color: #0e0e0e;
+            --card-background-color: #1b1b1b;
 		}
 	}
 </style>
