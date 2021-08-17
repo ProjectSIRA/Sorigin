3f1ad0bdfb8e4e3eed580b8bfdea2b70dbc3aeaf
Auros Nexus <aurosnex@gmail.com>
2021-08-17 02:29:25 -0400

    Did some stuff

--- sorigin-web/src/routes/index.svelte
+++ sorigin-web/src/routes/index.svelte
@@ -3,3 +3,11 @@
         <h1>Sorigin</h1>
     </main>
 </body>
+
+<style lang="css" global>
+	@media only screen and (prefers-color-scheme: dark) {
+		:root:not([data-theme="light"]) {
+            --background-color: #222222;
+		}
+	}
+</style>
