d0bc1c251e65bfad7d1deaca1a5ee2d38744b621
Auros Nexus <aurosnex@gmail.com>
2021-08-17 03:01:21 -0400

    Setup Navbar

---
+++ sorigin-web/src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<nav class="container-fluid">
+    <ul></ul>
+    <ul>
+        <li>Log In</li>
+    </ul>
+</nav>
+
+<slot></slot>
+
+<style lang="css" global>
+	@media only screen and (prefers-color-scheme: dark) {
+		:root:not([data-theme="light"]) {
+            --background-color: #222222;
+		}
+	}
+</style>
--- sorigin-web/src/routes/index.svelte
+++ sorigin-web/src/routes/index.svelte
@@ -1,13 +1,5 @@
 <body>
     <main class="container">
-        <h1>Sorigin</h1>
+        
     </main>
 </body>
-
-<style lang="css" global>
-	@media only screen and (prefers-color-scheme: dark) {
-		:root:not([data-theme="light"]) {
-            --background-color: #222222;
-		}
-	}
-</style>
