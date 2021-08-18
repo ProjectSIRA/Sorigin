af03fa9206c8fdc1f4b424c63b0767d635550217
Auros Nexus <aurosnex@gmail.com>
2021-08-18 01:08:00 -0400

    Update env.ts

--- sorigin-web/src/utils/env.ts
+++ sorigin-web/src/utils/env.ts
@@ -1,2 +1,3 @@
 export const SORIGIN_URL: string = import.meta.env.VITE_SORIGIN_URL as string
 export const REDIRECT_URL: string = import.meta.env.VITE_REDIRECT_URL as string
+export const REFETCH_URL: string = import.meta.env.VITE_REFETCH_URL as string
